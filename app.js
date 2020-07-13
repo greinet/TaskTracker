653101b0ab8bd2d793177d4e4d300df2441a7820
Andreas Greiner <andreasgreiner@online.de>
2020-07-13 16:37:30 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var cors = require("cors");
+var path = require('path');
+var fs = require('fs');
+var app = express();
+
+app.use(cors());
+
+
+app.get("/test", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
